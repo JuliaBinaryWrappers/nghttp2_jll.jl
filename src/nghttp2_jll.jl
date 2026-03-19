075760307f043d98c274e6d1ac12d4f5a51a1a5c
jlbuild <juliabuildbot@gmail.com>
2026-03-19 17:22:36 +0000

    nghttp2_jll build 1.68.1+0

--- src/nghttp2_jll.jl
+++ src/nghttp2_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("nghttp2")
-JLLWrappers.@generate_main_file("nghttp2", UUID("8e850ede-7688-5339-a07c-302acd2aaf8d"))
+JLLWrappers.@generate_main_file("nghttp2", Base.UUID("8e850ede-7688-5339-a07c-302acd2aaf8d"))
 end  # module nghttp2_jll
